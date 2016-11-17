1a4751d1a8086d5835864867d27019261dd51fc7
kaizen23 <damianis2@poczta.onet.pl>
2016-11-17 18:03:32 +0100

    Add shorter syntax

--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -21,10 +21,10 @@ namespace Introduction
 
         private static void ShowLargeFilesWithtLinq(string path)
         {
-            var query = from file in new DirectoryInfo(path).GetFiles()
-                        orderby file.Length descending
-                        select file;
-            foreach(var file in query.Take(5))
+            var query = new DirectoryInfo(path).GetFiles()
+                        .OrderByDescending(f => f.Length)
+                        .Take(5);
+            foreach(var file in query)
             {
                 Console.WriteLine($"{file.Name,-20} :{file.Length,10:N0}");
             }
